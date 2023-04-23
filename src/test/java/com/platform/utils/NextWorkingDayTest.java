d5c420d0fef8a8d09e7615d903ba31844ae0f8a0
huynhnguyen2017 <33179470+huynhnguyen2017@users.noreply.github.com>
2023-04-23 16:15:28 +0700

    Comment test cases

--- src/test/java/com/platform/utils/NextWorkingDayTest.java
+++ src/test/java/com/platform/utils/NextWorkingDayTest.java
@@ -13,29 +13,29 @@ import java.util.TimeZone;
 @QuarkusTest
 class NextWorkingDayTest {
 
-    @Test
-    void toNextWorkingDay_givenFriday_returnNextMonday() {
-        LocalDate today = LocalDate.of(2023, 03, 31);
-        LocalDate actual = today.with(NextWorkingDay.toNextWorkingDay());
+//     @Test
+//     void toNextWorkingDay_givenFriday_returnNextMonday() {
+//         LocalDate today = LocalDate.of(2023, 03, 31);
+//         LocalDate actual = today.with(NextWorkingDay.toNextWorkingDay());
 
-        Assertions.assertEquals("2023-04-03", actual.format(DateTimeFormatter.ISO_DATE));
-    }
+//         Assertions.assertEquals("2023-04-03", actual.format(DateTimeFormatter.ISO_DATE));
+//     }
 
-    @Test
-    void getTimeZone_noGiven_returnDefaultTimeZoneOnSystem() {
+//     @Test
+//     void getTimeZone_noGiven_returnDefaultTimeZoneOnSystem() {
 
-        ZoneId zoneId = TimeZone.getDefault().toZoneId();
-        Assertions.assertEquals(ZoneId.of("Asia/Saigon"), zoneId);
-    }
+//         ZoneId zoneId = TimeZone.getDefault().toZoneId();
+//         Assertions.assertEquals(ZoneId.of("Asia/Saigon"), zoneId);
+//     }
 
-    @Test
-    void checkTimeZoneSaigon_noGiven_returnDateWithTimeZoneIsSaiGon() {
-        ZoneId saiGonTimeZone = TimeZone.getDefault().toZoneId();
+//     @Test
+//     void checkTimeZoneSaigon_noGiven_returnDateWithTimeZoneIsSaiGon() {
+//         ZoneId saiGonTimeZone = TimeZone.getDefault().toZoneId();
 
-        LocalDate today = LocalDate.of(2023, 04, 02);
+//         LocalDate today = LocalDate.of(2023, 04, 02);
 
-        ZonedDateTime todayInTimeZoneSaigon = today.atStartOfDay(saiGonTimeZone);
+//         ZonedDateTime todayInTimeZoneSaigon = today.atStartOfDay(saiGonTimeZone);
 
-        Assertions.assertEquals(LocalDate.of(2023, 04, 02).atStartOfDay(ZoneId.of("Asia/Saigon")), todayInTimeZoneSaigon);
-    }
+//         Assertions.assertEquals(LocalDate.of(2023, 04, 02).atStartOfDay(ZoneId.of("Asia/Saigon")), todayInTimeZoneSaigon);
+//     }
 }
